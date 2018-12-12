7e26fe20b52fec6783c1509a8958013959e095a7
Matt DeSaix <mgdesaix@gmail.com>
2018-12-12 14:05:03 -0500

    Add files via upload

---
+++ Week2-Deques/src/Permutation.java
@@ -0,0 +1,18 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Permutation {
+	public static void main(String[] args) {
+		int k = Integer.parseInt(args[0]);
+		RandomizedQueue<String> rando = new RandomizedQueue<String>();
+		while (!StdIn.isEmpty()) {
+			String item = StdIn.readString();
+			rando.enqueue(item);
+		}
+		for (int i = 0; i < k; i++) {
+			String item2 = rando.dequeue();
+			StdOut.println(item2);
+			
+		}
+	}
+}
